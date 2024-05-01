4b0feb792eb07a61f12986c832ef514cbd49fd3a
Almatov-Nurs <nalmatov6@gmail.com>
2024-05-01 21:19:36 +0600

    add header

--- src/widgets/header/ui/index.jsx
+++ src/widgets/header/ui/index.jsx
@@ -9,16 +9,18 @@ import classes from './style.module.scss';
 
 const Header = () => {
   return (
-    <header id="header" className={`container-lg ${classes.header}`}>
-      <a href="/">Logo</a>
-      <ul>
-        {
-          links.map(({ title, link }, index) => (
-            <li key={index}><a href={link}>{title}</a></li>
-          ))
-        }
-      </ul>
-      <Button>Бронировать</Button>
+    <header id="header" className={classes.header}>
+      <nav className={`container-lg ${classes.nav}`}>
+        <a href="/">Logo</a>
+        <ul className={classes.links}>
+          {
+            links.map(({ title, link }, index) => (
+              <li key={index} className={classes.link}><a href={link}>{title}</a></li>
+            ))
+          }
+        </ul>
+        <Button>Бронировать</Button>
+      </nav>
     </header>
   );
 };
