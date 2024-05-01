afaa4ce0eb76de05851cab86ee4f4e6d7ab0f3bc
Almatov-Nurs <nalmatov6@gmail.com>
2024-05-01 22:19:39 +0600

    fix problems

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,17 @@
+// modules
+import url from '@/shared/assets/background.svg';
 // widgets
 import Header from "@/widgets/header/ui";
 import Footer from "@/widgets/footer/ui";
 // styles
 import classes from "./page.module.scss";
 
+
+const background = `top url('${url.src}') var(--background-clr)`;
+
 export default function Home() {
   return (
-    <main className={classes.main}>
+    <main className={classes.main} style={{ background }}>
       <Header/>
       <section id="hero">text</section>
       <Footer/>
